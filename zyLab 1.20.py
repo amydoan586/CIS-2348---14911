509b5cb757a508bba2631cad99b462b6834e42d6
Amy <amydoan586@gmail.com>
2020-08-30 16:22:26 -0500

    Added zyLab 1.20.py

---
+++ zyLab 1.20.py
@@ -0,0 +1,13 @@
+print("Enter integer:")
+user_num1 = int(input())
+print("You entered:", user_num1)
+user_NumSquared = user_num1 * user_num1
+user_NumCubed = user_num1 * user_num1 * user_num1
+print(user_num1, "squared is", user_NumSquared)
+print(user_num1, "cubed is", user_NumCubed, "!!")
+print("Enter another integer:")
+user_num2 = int(input())
+user_NumAdd = user_num1 + user_num2
+user_NumMultiple = user_num1 * user_num2
+print(user_num1, "+", user_num2, "is", user_NumAdd)
+print(user_num1, "*", user_num2, "is", user_NumMultiple)
