8d34c2b9384e830c03ef24d9883ead6af743cfd2
Amy <amydoan586@gmail.com>
2020-08-31 17:10:52 -0500

    Added Comments

--- zyLab 3.18.py
+++ zyLab 3.18.py
@@ -1,20 +1,20 @@
-import math
-Height = int(input("Enter wall height (feet):\n"))
-Width = int(input("Enter wall width (feet):\n"))
-Wall_Area = Height * Width
+import math #Add math functions
+Height = int(input("Enter wall height (feet):\n")) #User is asked to input height
+Width = int(input("Enter wall width (feet):\n")) #User is asked to input width
+Wall_Area = Height * Width #Calculate area of given height and width
 print("Wall area:", Wall_Area, "square feet")
-Paint_Needed = Wall_Area/350
-print("Paint needed:",'{:.2f}'.format(Paint_Needed),"gallons")
-Cans = math.ceil(Paint_Needed/1)
+Paint_Needed = Wall_Area/350 #Area is divided by 130 to get paint needed
+print("Paint needed:",'{:.2f}'.format(Paint_Needed),"gallons") #Two decimal places is needed for Paint_Needed
+Cans = math.ceil(Paint_Needed/1) #Calucating how many cans is needed and is rounded up to an integer
 print("Cans needed:",Cans, "can(s)\n")
-Color = input("Choose a color to paint the wall:\n")
-if Color == "red":
+Color = input("Choose a color to paint the wall:\n") #User is asked to input color
+if Color == "red": #Using if statements to calculate cost for red, blue, or green
     Cost = Cans * 35
     print("Cost of purchasing", Color, "paint: $"+ str(Cost))
 if Color == "blue":
     Cost = Cans * 25
-    print("Cost of purchasing", Color, "paint: $",str(Cost))
+    print("Cost of purchasing", Color, "paint: $"+ str(Cost))
 if Color == "green":
     Cost = Cans * 23
-    print("Cost of purchasing", Color, "paint: $",str(Cost))
+    print("Cost of purchasing", Color, "paint: $"+ str(Cost))
 
