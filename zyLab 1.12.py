8500d154a444af3e5f7e9278a3aa27078dc36df5
amydoan586 <70288844+amydoan586@users.noreply.github.com>
2020-09-06 23:23:59 -0500

    Delete zyLab 1.12.py

--- zyLab 1.12.py
+++
@@ -1,5 +0,0 @@
-#Amy Doan ID:1895125
-userNum = int(input()) #Asking user to input a number for userNum
-userNumSquared = userNum**2 #UserNum is squared and assign into userNumsquared
-print(userNumSquared, end = '\n') #Output userNumSquared with a new line
-
