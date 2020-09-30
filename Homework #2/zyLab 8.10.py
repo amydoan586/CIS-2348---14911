bb82c7a1c9daf2ab3f13ff87bf95feed4bc63384
Amy <amydoan586@gmail.com>
2020-09-30 17:00:27 -0500

    Added Comments 2

--- Homework #2/zyLab 8.10.py
+++ Homework #2/zyLab 8.10.py
@@ -1,5 +1,6 @@
 #Amy Doan ID:1895125
 string = str(input()) # User input string
+string = string.replace(" ","") # Remove white space
 if string == string[::-1]: # Compare string with reverse string
     print(string,"is a palindrome")
 else:
