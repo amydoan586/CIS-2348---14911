e5e786be333ea9ad900c7fa635aa749fdfa841ca
Amy <amydoan586@gmail.com>
2020-09-30 17:03:51 -0500

    added new variable for removing white space

--- Homework #2/zyLab 8.10.py
+++ Homework #2/zyLab 8.10.py
@@ -1,7 +1,7 @@
 #Amy Doan ID:1895125
 string = str(input()) # User input string
-string = string.replace(" ","") # Remove white space
-if string == string[::-1]: # Compare string with reverse string
+replaced_string = string.replace(" ","") # Remove white space
+if replaced_string == replaced_string[::-1]: # Compare string with reverse string
     print(string,"is a palindrome")
 else:
     print(string,"is not a palindrome")
