c6202c0f6ad3faf042e2e5e8c1ad857f51a05a53
Amy <amydoan586@gmail.com>
2020-09-30 16:55:43 -0500

    Added Comments

--- Homework #2/zyLab 8.10.py
+++ Homework #2/zyLab 8.10.py
@@ -1,6 +1,6 @@
 #Amy Doan ID:1895125
-string = str(input())
-if string == string[::-1]:
+string = str(input()) # User input string
+if string == string[::-1]: # Compare string with reverse string
     print(string,"is a palindrome")
 else:
     print(string,"is not a palindrome")
