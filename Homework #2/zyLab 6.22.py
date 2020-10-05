7bd5adb16d995b2c83ba0232f720ddf78f4645ad
Amy <amydoan586@gmail.com>
2020-10-05 14:11:11 -0500

    Added no solution

--- Homework #2/zyLab 6.22.py
+++ Homework #2/zyLab 6.22.py
@@ -1,16 +1,18 @@
 #Amy Doan ID:1895125
 # ax + by = c    dx + ey = f
-a = int(input())
-b = int(input())
+a = int(input()) # variable for x for line 1
+b = int(input()) # variable for y for line 1
 c = int(input())
-d = int(input())
-e = int(input())
+d = int(input()) # variable for x for line 2
+e = int(input()) # variable for y for line 1
 f = int(input())
+solution = False
 for x in range(-10,10):
     for y in range(-10,10):
         line1 = a*x + b*y
         line2 = d*x + e*y
         if (line1 == c and line2 == f):
             print(x,y)
-            break
-
+            solution = True
+if not solution: # Solution must be false to print No solution
+    print("No solution")
