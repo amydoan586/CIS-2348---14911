67929f9f6e1c39b79dce583d2eae324ff6865b31
Amy <amydoan586@gmail.com>
2020-11-28 17:45:06 -0600

    Added get_age() and valueerror

--- Homework #4/zyLab 12.7.py
+++ Homework #4/zyLab 12.7.py
@@ -1,11 +1,17 @@
 # Amy Doan ID: 1895125
-def fat_burning_heart_rate(Age):
+def fat_burning_heart_rate(age):
+    Heart_Rate = (220 - age) * .70
+    print("Fat burning heart rate for a",age, "year-old:",Heart_Rate,"bpm")
+    return Heart_Rate
+def get_age():
+    Age = int(input())
     if 18 < Age < 75:
-        Heart_Rate = (220 - Age) * .70
-        print("Fat burning heart rate for a",Age, "year-old:",Heart_Rate)
+        fat_burning_heart_rate(Age)
     else:
-        print("Invalid age.")
-        print("Could not calculate heart rate info.")
+        raise ValueError("Invalid age.")
 if __name__ == '__main__':
-    age = int(input("Enter age: \n"))
-    fat_burning_heart_rate(age)
+    try:
+        get_age()
+    except ValueError as Error:
+        print(Error)
+        print("Could not calculate heart rate info.\n")
